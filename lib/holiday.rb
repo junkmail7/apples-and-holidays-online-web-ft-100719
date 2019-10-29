9889bd8a2e5e8d1202acc72e143a3e42e5879d2b
William Pannell <lpann5@gmail.com>
2019-10-29 05:56:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holiday_hash.each do |k,v|
+  if v=="BBQ"
+    return k
+  end
+end
 end
 
 
